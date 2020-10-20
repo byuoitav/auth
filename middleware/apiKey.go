359fec3b39dcd0ef48475a3273745df184240698
Brayden Winterton <bwinterton@gmail.com>
2020-10-20 15:03:48 -0600

    Add AVAPIKey middleware

---
+++ middleware/apiKey.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+)
+
+const _avAPIKeyHeader = "x-av-access-key"
+
+type contextKey int
+
+const _avAPIKeyContextKey = 0
+
+// AVAPIKeymiddleware simply looks at the expected API Key header for AV products
+// and if an API Key is found it is placed into the context of the request
+func AVAPIKeyMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			apiKey := r.Header.Get(_avAPIKeyHeader)
+
+			// If the header is empty then continue
+			if apiKey == "" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			// Put the api key found into the context and continue
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, _avAPIKeyContextKey, apiKey)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		})
+	}
+}
+
+// GetAVAPIKey gets an AV API key from a context if it exists
+func GetAVAPIKey(ctx context.Context) (string, bool) {
+	apiKey, ok := ctx.Value(_avAPIKeyContextKey).(string)
+	return apiKey, ok
+}
